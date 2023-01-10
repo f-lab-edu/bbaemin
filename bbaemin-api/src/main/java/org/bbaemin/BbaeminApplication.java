6d6fdf8cb603a7f76ba8d5706e4c875f902b39c3
CHO CHAN HEE <cksgml4424@gmail.com>
2023-01-10 15:47:11 +0900

    gradle init commit -> multi module 설정 1. root directory build gradle 설정 2. bbaemin admin submodule 생성 2. settings gradle root, include 설정 #1

---
+++ bbaemin-api/src/main/java/org/bbaemin/BbaeminApplication.java
@@ -0,0 +1,10 @@
+package org.bbaemin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbaeminApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
