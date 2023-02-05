5eb4534fda1fe53492928481b34c8000f028f6d0
Cho Chan Hee <51773541+CHANEE-personal@users.noreply.github.com>
2023-02-05 15:51:03 +0900

    Item api (#10)
    * item api commit
    
    * put -> patch로 변경
    
    * 1. put으로 수정
    2. readme 수정

---
+++ bbaemin-api/src/main/java/org/bbaemin/item/controller/response/ItemResponse.java
@@ -0,0 +1,22 @@
+package org.bbaemin.item.controller.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class ItemResponse {
+
+    private String category;
+
+    private String store;
+
+    private String name;
+    private String description;
+    private int price;
+    private int quantity;
+
+    private List<ItemImageResponse> itemImageResponse;
+}
