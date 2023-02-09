2a66e0af9e63b5184d81699c26b41ff0d6e0b660
DESKTOP-0N7JBUG\user <younkyong94@gmail.com>
2023-02-10 01:54:10 +0900

    DeliveryFeeService - deliveryFee를 cartItemList로 조회하는 메소드로 변경

--- bbaemin-api/src/main/java/org/bbaemin/cart/service/DeliveryFeeService.java
+++ bbaemin-api/src/main/java/org/bbaemin/cart/service/DeliveryFeeService.java
@@ -1,9 +1,10 @@
 package org.bbaemin.cart.service;
 
 import lombok.RequiredArgsConstructor;
-import org.bbaemin.cart.vo.Cart;
 import org.bbaemin.cart.vo.CartItem;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,9 +13,13 @@ public class DeliveryFeeService {
     private static final int CRITERIA = 10000;
     private static final int BASIC_COST = 3000;
 
-    public int getDeliveryFee(Cart cart) {
-        int orderAmount = cart.getCartItemList().stream()
-                .mapToInt(CartItem::getTotalOrderPrice).sum();
+    public int getDeliveryFee(List<CartItem> cartItemList) {
+        int orderAmount = cartItemList.stream()
+                .mapToInt(cartItem -> cartItem.getOrderPrice() * cartItem.getOrderCount()).sum();
+        return getDeliveryFee(orderAmount);
+    }
+
+    public int getDeliveryFee(int orderAmount) {
         return orderAmount >= CRITERIA ? 0 : BASIC_COST;
     }
 }
