363708450c26b89c4fd809a8635267d373b54905
Cho Chan Hee <51773541+CHANEE-personal@users.noreply.github.com>
2023-02-15 10:19:48 +0900

    Category service, repository (#18)
    * 1. category 관련 service, repository
    2. service test case
    
    * 수정
    
    * repository method명 jpa에 맞게 수정
    
    * 1. category vo 추가
    2. create, update request builder 제거 후 VO에서 작업
    3. service test case 수정

--- bbaemin-api/src/main/java/org/bbaemin/category/controller/CategoryController.java
+++ bbaemin-api/src/main/java/org/bbaemin/category/controller/CategoryController.java
@@ -1,57 +1,58 @@
 package org.bbaemin.category.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.bbaemin.category.controller.request.CreateCategoryRequest;
+import org.bbaemin.category.controller.request.UpdateCategoryRequest;
 import org.bbaemin.category.controller.response.CategoryResponse;
+import org.bbaemin.category.service.CategoryService;
+import org.bbaemin.category.vo.Category;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/category")
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
     @GetMapping
     public List<CategoryResponse> listCategory() {
-        return List.of(CategoryResponse.builder()
-                .code("100")
-                .name("돼지고기")
-                .description("돼지고기")
-                .parent("육류")
-                .build());
+        return categoryService.listCategory();
     }
 
-    @GetMapping("/{idx}")
-    public CategoryResponse getCategory(@PathVariable Long idx) {
-        return CategoryResponse.builder()
-                .code("100")
-                .name("돼지고기")
-                .description("돼지고기")
-                .parent("육류")
-                .build();
+    @GetMapping("/{categoryId}")
+    public CategoryResponse getCategory(@PathVariable Long categoryId) {
+        return categoryService.getCategory(categoryId);
     }
 
     @PostMapping
     public CategoryResponse createCategory(@RequestBody CreateCategoryRequest createCategoryRequest) {
-        return CategoryResponse.builder()
-                .code("100")
-                .name("돼지고기")
-                .description("돼지고기")
-                .parent("육류")
+        Category category = Category.builder()
+                .code(createCategoryRequest.getCode())
+                .name(createCategoryRequest.getName())
+                .description(createCategoryRequest.getDescription())
+                .parentId(createCategoryRequest.getParentId())
                 .build();
+
+        return categoryService.createCategory(category);
     }
 
-    @PutMapping("/{idx}")
-    public CategoryResponse updateCategory(@PathVariable Long idx) {
-        return CategoryResponse.builder()
-                .code("100")
-                .name("소고기")
-                .description("소고기")
-                .parent("육류")
+    @PutMapping("/{categoryId}")
+    public CategoryResponse updateCategory(@PathVariable Long categoryId, @RequestBody UpdateCategoryRequest updateCategoryRequest) {
+        Category category = Category.builder()
+                .code(updateCategoryRequest.getCode())
+                .name(updateCategoryRequest.getName())
+                .description(updateCategoryRequest.getDescription())
+                .parentId(updateCategoryRequest.getParentId())
                 .build();
+
+        return categoryService.updateCategory(categoryId, category);
     }
 
-    @DeleteMapping("/{idx}")
-    public Long deleteCategory(@PathVariable Long idx) {
-        return idx;
+    @DeleteMapping("/{categoryId}")
+    public Long deleteCategory(@PathVariable Long categoryId) {
+        return categoryService.deleteCategory(categoryId);
     }
 }
