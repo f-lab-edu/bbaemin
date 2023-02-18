029984e697089b9e118f4af6f37afb364a13644f
CHO CHAN HEE <cksgml4424@gmail.com>
2023-02-18 19:31:07 +0900

    ApiResult 적용

--- bbaemin-api/src/main/java/org/bbaemin/category/controller/CategoryController.java
+++ bbaemin-api/src/main/java/org/bbaemin/category/controller/CategoryController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.bbaemin.category.domain.CategoryDto;
 import org.bbaemin.category.domain.CategoryEntity;
 import org.bbaemin.category.service.CategoryService;
+import org.bbaemin.config.response.ApiResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,27 +17,27 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<CategoryDto> listCategory() {
-        return categoryService.listCategory();
+    public ApiResult<List<CategoryDto>> listCategory() {
+        return ApiResult.ok(categoryService.listCategory());
     }
 
     @GetMapping("/{categoryId}")
-    public CategoryDto getCategory(@PathVariable Long categoryId) {
-        return categoryService.getCategory(categoryId);
+    public ApiResult<CategoryDto> getCategory(@PathVariable Long categoryId) {
+        return ApiResult.ok(categoryService.getCategory(categoryId));
     }
 
     @PostMapping
-    public CategoryDto createCategory(@RequestBody CategoryEntity categoryEntity) {
-        return categoryService.createCategory(categoryEntity);
+    public ApiResult<CategoryDto> createCategory(@RequestBody CategoryEntity categoryEntity) {
+        return ApiResult.ok(categoryService.createCategory(categoryEntity));
     }
 
     @PutMapping("/{categoryId}")
-    public CategoryDto updateCategory(@PathVariable Long categoryId, @RequestBody CategoryEntity categoryEntity) {
-        return categoryService.updateCategory(categoryId, categoryEntity);
+    public ApiResult<CategoryDto> updateCategory(@PathVariable Long categoryId, @RequestBody CategoryEntity categoryEntity) {
+        return ApiResult.ok(categoryService.updateCategory(categoryId, categoryEntity));
     }
 
     @DeleteMapping("/{categoryId}")
-    public Long deleteCategory(@PathVariable Long categoryId) {
-        return categoryService.deleteCategory(categoryId);
+    public ApiResult<Long> deleteCategory(@PathVariable Long categoryId) {
+        return ApiResult.ok(categoryService.deleteCategory(categoryId));
     }
 }
--- bbaemin-api/src/main/java/org/bbaemin/store/controller/StoreController.java
+++ bbaemin-api/src/main/java/org/bbaemin/store/controller/StoreController.java
@@ -1,6 +1,7 @@
 package org.bbaemin.store.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.bbaemin.config.response.ApiResult;
 import org.bbaemin.store.domain.StoreDto;
 import org.bbaemin.store.domain.StoreEntity;
 import org.bbaemin.store.service.StoreService;
@@ -17,27 +18,27 @@ public class StoreController {
     private final StoreService storeService;
 
     @GetMapping
-    public List<StoreDto> listStore() {
-        return storeService.listStore();
+    public ApiResult<List<StoreDto>> listStore() {
+        return ApiResult.ok(storeService.listStore());
     }
 
     @GetMapping("/{storeId}")
-    public StoreDto getStore(@PathVariable Long storeId) {
-        return storeService.getStore(storeId);
+    public ApiResult<StoreDto> getStore(@PathVariable Long storeId) {
+        return ApiResult.ok(storeService.getStore(storeId));
     }
 
     @PostMapping
-    public StoreDto createStore(@Valid @RequestBody StoreEntity storeEntity) {
-        return storeService.createStore(storeEntity);
+    public ApiResult<StoreDto> createStore(@Valid @RequestBody StoreEntity storeEntity) {
+        return ApiResult.ok(storeService.createStore(storeEntity));
     }
 
     @PutMapping("/{storeId}")
-    public StoreDto updateStore(@PathVariable Long storeId, @Valid @RequestBody StoreEntity storeEntity) {
-        return storeService.updateStore(storeId, storeEntity);
+    public ApiResult<StoreDto> updateStore(@PathVariable Long storeId, @Valid @RequestBody StoreEntity storeEntity) {
+        return ApiResult.ok(storeService.updateStore(storeId, storeEntity));
     }
 
     @DeleteMapping("/{storeId}")
-    public Long deleteStore(@PathVariable Long storeId) {
-        return storeService.deleteStore(storeId);
+    public ApiResult<Long> deleteStore(@PathVariable Long storeId) {
+        return ApiResult.ok(storeService.deleteStore(storeId));
     }
 }
