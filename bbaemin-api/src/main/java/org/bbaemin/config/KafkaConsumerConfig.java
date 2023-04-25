e2091e43160245622436fad0819f0d1cbf87c688
DESKTOP-0N7JBUG\user <younkyong94@gmail.com>
2023-04-26 07:06:49 +0900

    commit

---
+++ bbaemin-api/src/main/java/org/bbaemin/config/KafkaConsumerConfig.java
@@ -0,0 +1,20 @@
+package org.bbaemin.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    public ConsumerFactory<String, OrderCompletedMessage> orderMessageConsumerFactory() {
+
+    }
+}
